2c06fb93ec339eb3be6ce1b7421bb811738a8202
Jonas Leite <jonas.programador01@gmail.com>
2024-02-29 17:46:11 -0300

    deploy da API

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+const port = 3090;
+
+
+app.get('/user',(req,res)=>{
+    res.status(201).json({message:'Jonas me adota?'})
+})
+app.use(express.json())
+
+
+app.listen(port,()=> console.log(`Server running in port ${port}`))
