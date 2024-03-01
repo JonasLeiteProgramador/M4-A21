5a4dbdb6b2f780047aca47579366101703a7c9ef
Jonas Leite <jonas.programador01@gmail.com>
2024-02-29 21:06:17 -0300

    deploy da API

---
+++ src/Message.class.js
@@ -0,0 +1,10 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export class Message {
+    constructor(name, content){
+        this.id = uuidv4();
+        this.name = name;
+        this.content = content;
+        this.created_at = new Date().toLocaleTimeString();
+    }
+}
--- src/server.js
+++ src/server.js
@@ -1,14 +1,59 @@
 import express from "express";
+import { Message } from "./Message.class.js";
 
 const app = express();
 
 const port = 3090;
 
+let messages = []
 
-app.get('/message',(req,res)=>{
-    res.status(201).send('Jonas me adota?')
-})
 app.use(express.json())
+
+
+let createMessage = (name,content) => {
+    
+  const newMessage = new Message(name,content)
+  messages.push(newMessage)
+  return newMessage
+}
+let updateMessage = (id,content) => {
+    const message = messages.findIndex(message => message.id === id )
+    const updatedMessage = messages[message].content == content 
+    return updatedMessage
+  }
+
+  let deleteMessage = id => {
+    const message = messages.findIndex(message => message.id === id )
+    const deletedMessage =  messages.splice(messages[message],1)
+    return deletedMessage
+  }
+app.get('/messages',(req,res)=>{
+    res.status(201).json({messages})
+})
+
+app.post('/create-message',(req,res)=>{
+    const {name,content } = req.body;
+    const newMessage = createMessage(name,content)
+    res.status(201).json({message:'Messagem criada com sucesso!',newMessage})
+})
+app.patch('/update-message/:id',(req,res)=>{
+    const {id} = req.params
+    const {content } = req.body;
+    const updatedMessage = updateMessage(id,content)
+    res.status(201).json({message:'Messagem criada com sucesso!'})
+})
+
+app.delete('/delete-message/:id',(req,res)=>{
+    const {id} = req.params;
+    const deletedMessage = deleteMessage(id);
+    res.status(201).json({message:'Messagem apagada com sucesso!',deletedMessage})
+});
+
+
+
+
+
+
 
 
 app.listen(port,()=> console.log(`Server running in port ${port}`))
